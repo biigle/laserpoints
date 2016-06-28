b13b1e56341964d10736a0d64d6201fd4ae53dfa
Martin Zurowietz <martin@zurowietz.de>
2016-06-28 12:53:04 +0200

    Implement user permissions in the UI

--- src/resources/views/imagesIndex.blade.php
+++ src/resources/views/imagesIndex.blade.php
@@ -2,7 +2,7 @@
     <script src="{{ asset('vendor/laserpoints/scripts/main.js') }}"></script>
 @endpush
 <div class="col-sm-6 col-lg-4">
-    <div class="panel panel-default" data-ng-app="dias.laserpoints" data-ng-controller="LaserpointsController">
+    <div class="panel panel-default">
         <div class="panel-heading">
             <h3 class="panel-title">Laserpoints</h3>
         </div>
@@ -11,21 +11,21 @@
             <table class="table">
                 @if ($img->px)
                     <tr>
-                        <th>#Pixels</th>
+                        <th>Number of pixels</th>
                         <td>{{ $img->px }}</td>
                     </tr>
                 @endif
 
                 @if ($img->area)
                     <tr>
-                        <th>Area</th>
+                        <th>Area covered by the image</th>
                         <td>{{ round($img->area, 2) }} m²</td>
                     </tr>
                 @endif
 
                 @if ($img->count)
                     <tr>
-                        <th>#Laserpoints</th>
+                        <th>Number of laserpoints</th>
                         <td>{{ $img->count }}</td>
                     </tr>
                 @endif
@@ -45,7 +45,7 @@
                 @endif
             </table>
         @endif
-        <div class="panel-body">
+        <div class="panel-body" data-ng-app="dias.laserpoints" data-ng-controller="LaserpointsController">
             @if (!$img->laserpoints)
                 <div class="alert alert-info" data-ng-hide="isSubmitted()">
                     No laserpoint detection was performed yet.
@@ -55,26 +55,28 @@
                     The automatic laserpoint detection failed. Please annotate laserpoints manually.
                 </div>
             @endif
-            <form class="form-inline" data-ng-hide="isSubmitted()">
-                @if($img->laserpoints)
-                    <div class="form-group">
-                        <input class="form-control" data-ng-model="distance" id="distance" type="number" placeholder="New laser distance in cm" title="Distance between two laserpoints in cm. Leave empty to use the previously set distance ({{$img->distance}})"></input>
-                    </div>
-                    <div class="form-group">
-                        <button class="btn btn-success" data-ng-disabled="isComputing()" data-ng-click="reDetection({{$image->id}}, {{$img->distance}})" title="Restart the laserpoint detection">Submit</button>
-                    </div>
-                @else
-                    <div class="form-group">
-                        <input class="form-control" data-ng-model="distance" id="distance" type="number" placeholder="Laser distance in cm" title="Distance between two laserpoints in cm" required></input>
-                    </div>
-                    <div class="form-group">
-                        <button class="btn btn-success" data-ng-disabled="isComputing()" data-ng-click="newDetection({{$image->id}})" title="Start a new laserpoint detection">Submit</button>
-                    </div>
-                @endif
-            </form>
-            <div class="alert alert-success ng-cloak ng-hide" data-ng-show="isSubmitted()">
-                The laserpoint detection was submitted and will be available soon.
-            </div>
+            @can('add-annotation', $image)
+                <form class="form-inline" data-ng-hide="isSubmitted()">
+                    @if($img->laserpoints)
+                        <div class="form-group">
+                            <input class="form-control" data-ng-model="distance" id="distance" type="number" placeholder="New laser distance in cm" title="Distance between two laserpoints in cm. Leave empty to use the previously set distance ({{$img->distance}})"></input>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-success" data-ng-disabled="isComputing()" data-ng-click="reDetection({{$image->id}}, {{$img->distance}})" title="Restart the laserpoint detection">Submit</button>
+                        </div>
+                    @else
+                        <div class="form-group">
+                            <input class="form-control" data-ng-model="distance" id="distance" type="number" placeholder="Laser distance in cm" title="Distance between two laserpoints in cm" required></input>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-success" data-ng-disabled="isComputing()" data-ng-click="newDetection({{$image->id}})" title="Start a new laserpoint detection">Submit</button>
+                        </div>
+                    @endif
+                </form>
+                <div class="alert alert-success ng-cloak ng-hide" data-ng-show="isSubmitted()">
+                    The laserpoint detection was submitted and will be available soon.
+                </div>
+            @endcan
         </div>
     </div>
 </div>
--- src/resources/views/transectsMenubar.blade.php
+++ src/resources/views/transectsMenubar.blade.php
@@ -1,19 +1,20 @@
-<div data-ng-controller="LaserpointsController">
-    <button class="btn btn-default transect-menubar__item" data-popover-placement="right" data-uib-popover-template="'laserpointsPopover.html'" type="button" title="Compute the area of each image in this transect">
-        <span class="glyphicon glyphicon-sound-stereo" aria-hidden="true"></span>
-    </button>
-</div>
-<script type="text/ng-template" id="laserpointsPopover.html">
-    <form class="" data-ng-hide="isSubmitted()">
-        <div class="form-group">
-            <input data-ng-model="data.distance" type="number" id="distance" title="Distance between two laserpoints in cm" placeholder="Laser distance in cm" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <button data-ng-click="newDetection()" class="btn btn-success" title="Compute the area of each image in this  transect." data-ng-disabled="isComputing()">Submit</button>
-        </div>
-    </form>
-    <div class="alert alert-success ng-hide" data-ng-show="isSubmitted()">
-        The laserpoint detection was submitted and will be available soon.
+@can('edit-in', $transect)
+    <div data-ng-controller="LaserpointsController">
+        <button class="btn btn-default transect-menubar__item" data-popover-placement="right" data-uib-popover-template="'laserpointsPopover.html'" type="button" title="Compute the area of each image in this transect">
+            <span class="glyphicon glyphicon-sound-stereo" aria-hidden="true"></span>
+        </button>
     </div>
-</script>
-
+    <script type="text/ng-template" id="laserpointsPopover.html">
+        <form class="" data-ng-hide="isSubmitted()">
+            <div class="form-group">
+                <input data-ng-model="data.distance" type="number" id="distance" title="Distance between two laserpoints in cm" placeholder="Laser distance in cm" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <button data-ng-click="newDetection()" class="btn btn-success" title="Compute the area of each image in this  transect." data-ng-disabled="isComputing()">Submit</button>
+            </div>
+        </form>
+        <div class="alert alert-success ng-hide" data-ng-show="isSubmitted()">
+            The laserpoint detection was submitted and will be available soon.
+        </div>
+    </script>
+@endcan
