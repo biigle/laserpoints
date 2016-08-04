81434a2b368bff913311fc7ba4a06f25ac0f38cd
Martin Zurowietz <martin@zurowietz.de>
2016-08-04 10:22:01 +0200

    Improve laserpoint opacity slider

--- src/resources/views/annotationsSettings.blade.php
+++ src/resources/views/annotationsSettings.blade.php
@@ -1,6 +1,6 @@
 <h4>Laserpoints</h4>
 
 <div class="settings-foldout__item" data-ng-controller="LaserpointsSettingsController">
-    <label title="Show automatically detected laserpoints">Laserpoint opacity (<span data-ng-bind="settings.laserpoint_opacity | number:2"></span>)</label>
+    <label title="Show automatically detected laserpoints">Opacity <span class="ng-cloak" data-ng-switch="settings.laserpoint_opacity!=='0'">(<span data-ng-switch-when="true" data-ng-bind="settings.laserpoint_opacity | number:2"></span><span data-ng-switch-default="">hidden</span>)</span></label>
     <input type="range" min="0" max="1" step="0.01" data-ng-model="settings.laserpoint_opacity">
 </div>
