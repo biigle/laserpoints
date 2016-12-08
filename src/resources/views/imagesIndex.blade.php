2120aeeaef7a3ab769dde0a5ce3bb53e77160ac4
Martin Zurowietz <martin@zurowietz.de>
2016-12-08 11:38:55 +0100

    Update images index view mixin for new layout

--- src/resources/views/imagesIndex.blade.php
+++ src/resources/views/imagesIndex.blade.php
@@ -4,7 +4,7 @@
     @endpush
 @endunless
 
-<div class="col-sm-6 col-lg-4">
+<div class="col-sm-12 col-lg-6">
     <div class="panel panel-default">
         <div class="panel-heading">
             <h3 class="panel-title">Laserpoints</h3>
