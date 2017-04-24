9c7e13b448a0c7b85e19ca3e68bfac784f5d16c8
Martin Zurowietz <martin@zurowietz.de>
2017-04-24 13:36:06 +0200

    Fix swapped laserpoints coordinates in delphiGather script

--- src/resources/scripts/delphiGather.py
+++ src/resources/scripts/delphiGather.py
@@ -28,7 +28,7 @@ Expected input file format:
 
 filePrefix = js['filePrefix']
 manLaserpoints = js['manLaserpoints']
-manLaserpoints = np.fliplr(np.array(manLaserpoints))
+manLaserpoints = np.array(manLaserpoints)
 manLaserpointFiles = [filePrefix + '/' + file for file in js['manLaserpointFiles']]
 output = js['tmpFile']
 
@@ -44,10 +44,10 @@ maskImage = np.zeros([tmpimg.shape[0], tmpimg.shape[1]], bool)
 for idx, i in enumerate(manLaserpoints):
     lpimg = imread(manLaserpointFiles[idx])
     for j in i:
-        maskImage[max(0, j[0] - delta1):min(j[0] + delta1, maskImage.shape[0]), max(0, j[1] - delta1):min(j[1] + delta1, maskImage.shape[1])] = 1
+        maskImage[max(0, j[1] - delta1):min(j[1] + delta1, maskImage.shape[0]), max(0, j[0] - delta1):min(j[0] + delta1, maskImage.shape[1])] = 1
         # get color of lp
         # save color and x,y to array
-        lps.append(lpimg[j[0], j[1]])
+        lps.append(lpimg[j[1], j[0]])
 np.savez_compressed(output, maskImage=maskImage, lps=lps, manLaserpoints=manLaserpoints)
 # rename the file because stupid numpy always appends a '.npz' to the file name
 os.rename(output + '.npz', output)
