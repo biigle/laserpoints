96c9db0638bf0237c19a1f8dd42b7583e11891eb
Martin Zurowietz <martin@zurowietz.de>
2017-05-18 17:56:39 +0200

    Simplify directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var shell   = require('gulp-shell');
 elixir(function (mix) {
    process.chdir('src');
    // mix.sass('main.scss', 'public/assets/styles/main.css');
-    mix.angular('resources/assets/js/vue/', 'public/assets/scripts', 'main.js');
+    mix.angular('resources/assets/js/', 'public/assets/scripts', 'main.js');
     mix.task('publish', 'public/assets/**/*');
 });
 
