ed5776e426f04a5be190a734beb8056fc766d857
Martin Zurowietz <martin@zurowietz.de>
2017-05-19 13:42:10 +0200

    Remove obsolete AngularJS attribute

--- src/resources/views/annotationsSettingsTab.blade.php
+++ src/resources/views/annotationsSettingsTab.blade.php
@@ -1,6 +1,6 @@
 @if (\Biigle\Modules\Laserpoints\Volume::convert($volume)->hasDetectedLaserpoints())
 <component :is="plugins.laserPoints" :settings="settings" inline-template>
-    <div class="settings-tab__section" data-ng-controller="LaserpointsSettingsController">
+    <div class="settings-tab__section">
         <label>Laser points opacity (<span v-if="shown" v-text="opacity"></span><span v-else>hidden</span>)</label>
         <input type="range" min="0" max="1" step="0.1" v-model="opacityValue">
     </div>
