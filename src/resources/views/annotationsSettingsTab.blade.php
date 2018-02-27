59e9b332343bf55108c9a4507e8b17dc544a506c
Martin Zurowietz <martin@zurowietz.de>
2018-02-27 16:21:52 +0100

    Capitalize annotation tool sidebar plugin title

--- src/resources/views/annotationsSettingsTab.blade.php
+++ src/resources/views/annotationsSettingsTab.blade.php
@@ -1,7 +1,7 @@
 @if (\Biigle\Modules\Laserpoints\Volume::convert($volume)->hasDetectedLaserpoints())
 <component :is="plugins.laserPoints" :settings="settings" inline-template>
     <div class="sidebar-tab__section">
-        <h5>Laser points opacity (<span v-if="shown" v-text="opacity"></span><span v-else>hidden</span>)</h5>
+        <h5>Laser Points Opacity (<span v-if="shown" v-text="opacity"></span><span v-else>hidden</span>)</h5>
         <input type="range" min="0" max="1" step="0.1" v-model="opacityValue">
     </div>
 </component>
